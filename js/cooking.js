3653384e87e5cb34dd586113e551850ae70f8c4c
Tony <tonywon89@gmail.com>
2015-06-12 17:29:01 -0400

    Add fade in and fade out for images, with some new post functions

--- js/cooking.js
+++ js/cooking.js
@@ -1,9 +1,27 @@
 
 var newBtn = document.getElementById("submit-btn");
 
-var newPostWind = function () { 
-	alert("This is not functional yet")
+var dishImage;
+var dishName;
+var dishDescript;
+
+var makeNewPost = function () { 
+	dishImage = $('dishImg').val();
+	dishName = $('#dishName').val();
+	dishDescript = $('#dishDescription').val();
+	$('#main').append("<h3>" + dishName + "</h3>");
+	$('#main').append("<p>" + dishDescript + "</p>");
 }
 
-newBtn.addEventListener("click", newPostWind)
+newBtn.addEventListener("click",makeNewPost)
+
+
+$('img').hover(
+	function() {
+		$(this).fadeTo(400,0);
+	},
+	function() {
+		$(this).fadeTo(400, 1);
+	}
+);
 
