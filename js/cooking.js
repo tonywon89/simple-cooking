ec8a69cc15e79e194d17be8584f65fc78805e5fb
Tony <tonywon89@gmail.com>
2015-06-15 14:22:45 -0400

    Update "New Post" button functionality to add new rows and columns

--- js/cooking.js
+++ js/cooking.js
@@ -1,27 +1,68 @@
+$(document).ready(function(){
+	
 
-var newBtn = document.getElementById("submit-btn");
+	var dishImage;
+	var dishName;
+	var dishDescript;
+	var rowId = 1;
+	counter = 0;
 
-var dishImage;
-var dishName;
-var dishDescript;
+	var makeNewPost = function () { 
+		dishImage = $('#dishImg').val();
+		dishName = $('#dishName').val();
+		dishDescript = $('#dishDescription').val();
+		
+		if (counter == 0) {
+			var entry = " \
+			<div class='row' id='dish" + rowId + "'>  \
+				<div class='col-md-6'> \
+					<img src='" + dishImage + "' alt='Dish Image'> \
+					<h3>" + dishName + "</h3> \
+					<p>" + dishDescript + "</p> \
+				</div> \
+			</div> \
+			" 
+			$('#main').append(entry);
+			counter += 1;
+		} else {
+			var entry = " \
+			<div class='col-md-6'> \
+					<img src='" + dishImage + "' alt='Dish Image'> \
+					<h3>" + dishName + "</h3> \
+					<p>" + dishDescript + "</p> \
+				</div> \
+			</div> \
+			"
 
-var makeNewPost = function () { 
-	dishImage = $('dishImg').val();
-	dishName = $('#dishName').val();
-	dishDescript = $('#dishDescription').val();
-	$('#main').append("<h3>" + dishName + "</h3>");
-	$('#main').append("<p>" + dishDescript + "</p>");
-}
-
-newBtn.addEventListener("click",makeNewPost)
-
-
-$('img').hover(
-	function() {
-		$(this).fadeTo(400,0);
-	},
-	function() {
-		$(this).fadeTo(400, 1);
+			$('#dish' + rowId).append(entry);
+			
+			
+			rowId += 1;
+			counter -= 1;
+		}
 	}
-);
+
+	$('#submit-btn').click(makeNewPost);
+	
+	/*
+	$('img').hover(
+		function() {
+			$(this).fadeTo(400,0);
+		},
+		function() {
+			$(this).fadeTo(400, 1);
+		}
+	);
+	*/
+
+	$(document).on("mouseenter", "img", function(){
+		$(this).fadeTo(500,0.25);
+	});
+
+	$(document).on("mouseleave", "img", function(){
+		$(this).fadeTo(500,1);
+	});
+		
+	
+});
 
